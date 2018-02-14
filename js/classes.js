8dd1e14bf2359723d76e7a3021e7b21d9b3e2103
Alexandre-Furstenberger <alexandre.furstenberger@gmail.com>
2018-02-14 21:09:58 -0200

    refactor: Remove unecessary methods

--- js/classes.js
+++ js/classes.js
@@ -4,12 +4,8 @@
    * Created by Alexandre Furstenberger
    * Date: February 2018
    * Project: developed as a project for Udacity's Front End Web Developer Nanodegree
-   * Using jQuery v3.3.1
 
-*/
-
-/* Sections
-   ======================================================================================== */
+*  ------------------------------------------------------------------/
 
 /**
 * @description Represents a grid card
@@ -18,12 +14,10 @@
 * @param {string} id - Card identification
 */
 
-
 let gridCard = function (icon, id){
 
     this.cardIcon = icon;               // assign which icon object will show once faced up
     this.cardID = id;                   // identify object 
-    this.cardColor = 'darkslategray';   // define default card color faced down
     this.isClicked = false;             // define card status if clicked
     this.isMatched = false;             // define card status if matched
 
@@ -31,49 +25,52 @@ let gridCard = function (icon, id){
 
 /* --------------- read functions --------------- */
 
-// get access to card icon value
+/**
+* @description Gets card icon value
+* @returns {number} icon value
+*/
 gridCard.prototype.getCardIcon = function(){
     return this.cardIcon;    
 }
 
-// get card ID
+/**
+* @description Gets card ID
+* @returns {number} card ID
+*/
 gridCard.prototype.getCardID = function () {
     return this.cardID;
 }
 
-// get card color
-gridCard.prototype.getCardColor = function () {
-    return this.cardColor;
-}
-
-// get card clicked status
+/**
+* @description Gets card clicked status
+* @returns {bool} is clicked flag
+*/
 gridCard.prototype.cardClicked = function () {
     return this.isClicked;
 }
 
+/**
+* @description Gets card matched status
+* @returns {bool} is matched flag
+*/
 // get card mathed status
 gridCard.prototype.cardMatched = function () {
     return this.isMatched;
 }
 
 /* --------------- write functions -------------- */
-
-// change card icon text
-gridCard.prototype.setCardText = function (icon) {
-    this.cardIcon = icon;
-}
-
-// change card color
-gridCard.prototype.setCardColor = function (color) {
-    this.cardColor = color;
-}
-
-// change card clicked status
+/**
+* @description Change card clicked status
+* @param {bool} status flag
+*/
 gridCard.prototype.setClickedStatus = function(status){
     this.isClicked = status;
 }
 
-// change card matched status
+/**
+* @description Change card matched status
+* @param {bool} status flag
+*/
 gridCard.prototype.setMatchedStatus = function (status) {
     this.isMatched = status;
 }
